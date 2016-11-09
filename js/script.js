9b0cc4e9b45aea1f47c97abb1913ac0d3e3387d0
Suyeol Jeon <devxoul@gmail.com>
2016-11-10 04:24:44 +0900

    다음 지도 이동/확대/축소를 막음

--- js/script.js
+++ js/script.js
@@ -5,14 +5,17 @@ function drawMap() {
     level: 3
   };
   var map = new daum.maps.Map(container, options);
+  map.setDraggable(false);
+  map.setZoomable(false);
+
   var markerPosition  = new daum.maps.LatLng(33.450701, 126.570667); 
   var marker = new daum.maps.Marker({
       position: markerPosition
   });
   marker.setMap(map);
-  // var iwContent = '<div style="padding:5px;">Hello World! <br><a href="http://map.daum.net/link/map/Hello World!,33.450701,126.570667" style="color:blue" target="_blank">큰지도보기</a> <a href="http://map.daum.net/link/to/Hello World!,33.450701,126.570667" style="color:blue" target="_blank">길찾기</a></div>';
+
   var iwContent = $('#info-window-template').html();
-  var iwPosition = new daum.maps.LatLng(33.450701, 126.570667); //인포윈도우 표시 위치입니다
+  var iwPosition = new daum.maps.LatLng(33.450701, 126.570667);
   var infowindow = new daum.maps.InfoWindow({
       position : iwPosition, 
       content : iwContent 
